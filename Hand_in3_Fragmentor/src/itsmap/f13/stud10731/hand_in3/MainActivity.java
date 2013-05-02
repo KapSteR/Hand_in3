e136477933b179224067b0ce4570658e0c7077e2
René Nilsson <renenilsson01@gmail.com>
2013-05-02 21:15:33 +0200

    Added callback interface from MenuFragment to MainActivity

--- Hand_in3_Fragmentor/src/itsmap/f13/stud10731/hand_in3/MainActivity.java
+++ Hand_in3_Fragmentor/src/itsmap/f13/stud10731/hand_in3/MainActivity.java
@@ -1,26 +1,32 @@
 package itsmap.f13.stud10731.hand_in3;
 
+import itsmap.f13.stud10731.hand_in3.MenuFragment.OnMenuFragmentClickListener;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
+import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity {
-	
-//	ArrayList<String> listItems = new ArrayList<String>();
-//	ArrayAdapter<String> adapter;
+public class MainActivity extends FragmentActivity implements OnMenuFragmentClickListener {
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        FragmentManager fragmentManager = getSupportFragmentManager();
-
-        FragmentTransaction fragTrans = fragmentManager.beginTransaction();
-        fragTrans.add(R.id.fragment1, new MenuFragment());
-        fragTrans.commit();
         setContentView(R.layout.activity_main);
+        
+        if (findViewById(R.id.fragment_container) != null) {
+        
+        	if (savedInstanceState != null) {
+                return;
+            }
+        	
+	        FragmentManager fragmentManager = getSupportFragmentManager();
+	
+	        FragmentTransaction fragTrans = fragmentManager.beginTransaction();
+	        fragTrans.add(R.id.fragment_container, new MenuFragment());
+	        fragTrans.commit();
+        }
     }
 
 
@@ -30,5 +36,13 @@ public class MainActivity extends FragmentActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+	@Override
+	public void onMenuFragmentClick(String item) {
+		Toast.makeText(getBaseContext(), item, Toast.LENGTH_SHORT).show();
+	}
+
+
     
 }
--- Hand_in3_Fragmentor/src/itsmap/f13/stud10731/hand_in3/MenuFragment.java
+++ Hand_in3_Fragmentor/src/itsmap/f13/stud10731/hand_in3/MenuFragment.java
@@ -1,16 +1,34 @@
 package itsmap.f13.stud10731.hand_in3;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MenuFragment extends ListFragment {
 	
-//	public interface MenuFragmentCallback {
-//		public void onMenuFragmentCallback();
-//	}
+	OnMenuFragmentClickListener mCallback;
+	
+	public interface OnMenuFragmentClickListener {
+		public void onMenuFragmentClick(String item);
+	}
+	
+	@Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (OnMenuFragmentClickListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnMenuFragmentClickListener");
+        }
+    }
 	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
@@ -23,7 +41,7 @@ public class MenuFragment extends ListFragment {
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		String item = (String)getListAdapter().getItem(position);
-		
+		mCallback.onMenuFragmentClick(item);
 	}
 	
 	
