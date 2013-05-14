348d7db44ca0e481461ffa91173f75333565f1ee
René Nilsson <renenilsson01@gmail.com>
2013-05-14 10:05:00 +0200

    Some updates....

--- Hand_in4/src/com/kn10731/iha/handin4/DownloadService.java
+++ Hand_in4/src/com/kn10731/iha/handin4/DownloadService.java
@@ -20,16 +20,17 @@ import org.json.JSONObject;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 public class DownloadService extends Service {
-	
+
 	private final String TAG = "downloadService";
 	private final IBinder mBinder = new LocalBinder();
 	List<String> sTogList = null;
-	
+
 	public class LocalBinder extends Binder {
 		DownloadService getService() {
 			return DownloadService.this;
@@ -41,65 +42,72 @@ public class DownloadService extends Service {
 		return mBinder;
 	}
 
-	public void downloadList() {  //must run on a background thread
-
-		String result = null;
-		URI myURI = null;
-
-		try {
-			myURI = new URI("http://stog.itog.dk/itog/action/list/format/json");
-		} catch (URISyntaxException e) {
-			// Deal with it
-		}
-		HttpClient httpClient = new DefaultHttpClient();
-		HttpGet getMethod = new HttpGet(myURI);
-		HttpResponse webServerResponse = null;
-		try {
-			webServerResponse = httpClient.execute(getMethod);
-		} catch (ClientProtocolException e) {
-		} catch (IOException e) {
-		}
-
-		HttpEntity httpEntity = webServerResponse.getEntity();
-
-		if (httpEntity != null) {
-			InputStream instream;
-			try {
-				instream = httpEntity.getContent();
-				result = convertStreamToString(instream);
-				instream.close();
-			} catch (IllegalStateException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		}
-
-		JSONObject myJSON = null;
-
-		try {
-			myJSON = new JSONObject(result);
-			JSONArray array = myJSON.getJSONArray("items");
-			sTogList.clear();
-			
-			for (int i = 0; i < array.length(); i++) {
-				JSONObject sTogJSON = array.getJSONObject(i);
-
-				String name = sTogJSON.getString("name");
-				sTogList.add(name);
-				Log.d(TAG, name);
-
-			}
-		} catch (JSONException e) {
-		}
-
-	}
-	
-	public List<String> getDownloadedList(){
+	public List<String> getDownloadedList() {
 		return sTogList;
+	}
+
+	public class DownloadTask extends AsyncTask<String, Void, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+
+			String result = null;
+			URI myURI = null;
+
+			try {
+				myURI = new URI(
+						"http://stog.itog.dk/itog/action/list/format/json");
+			} catch (URISyntaxException e) {
+				// Deal with it
+			}
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpGet getMethod = new HttpGet(myURI);
+			HttpResponse webServerResponse = null;
+			try {
+				webServerResponse = httpClient.execute(getMethod);
+			} catch (ClientProtocolException e) {
+			} catch (IOException e) {
+			}
+
+			HttpEntity httpEntity = webServerResponse.getEntity();
+
+			if (httpEntity != null) {
+				InputStream instream;
+				try {
+					instream = httpEntity.getContent();
+					result = convertStreamToString(instream);
+					instream.close();
+				} catch (IllegalStateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+
+			JSONObject myJSON = null;
+
+			try {
+				myJSON = new JSONObject(result);
+				JSONArray array = myJSON.getJSONArray("items");
+				sTogList.clear();
+
+				for (int i = 0; i < array.length(); i++) {
+					JSONObject sTogJSON = array.getJSONObject(i);
+
+					String name = sTogJSON.getString("name");
+					sTogList.add(name);
+					Log.d(TAG, name);
+
+				}
+			} catch (JSONException e) {
+			}
+
+			// send Broadcast
+			return null;
+		}
 	}
 
 	public String convertStreamToString(InputStream is) throws IOException {
--- Hand_in4/src/com/kn10731/iha/handin4/MainActivity.java
+++ Hand_in4/src/com/kn10731/iha/handin4/MainActivity.java
@@ -11,44 +11,42 @@ import android.os.IBinder;
 import com.kn10731.iha.handin4.DownloadService.LocalBinder;
 
 public class MainActivity extends ListActivity {
-	
+
 	DownloadService mService;
-    boolean mBound = false;
-	
+	boolean mBound = false;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-	
+
 	@Override
-    protected void onStart() {
-        super.onStart();
-        // Bind to LocalService
-        Intent intent = new Intent(this, DownloadService.class);
-        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-    }
-	
-	
-	 /** Defines callbacks for service binding, passed to bindService() */
-    private ServiceConnection mConnection = new ServiceConnection() {
+	protected void onStart() {
+		super.onStart();
+		// Bind to LocalService
+		Intent intent = new Intent(this, DownloadService.class);
+		bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+	}
 
-        @Override
-        public void onServiceConnected(ComponentName className,
-                IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
-            LocalBinder binder = (LocalBinder) service;
-            mService = binder.getService();
-            mBound = true;
-        }
+	/** Defines callbacks for service binding, passed to bindService() */
+	private ServiceConnection mConnection = new ServiceConnection() {
 
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            mBound = false;
-        }
-    };
-    
-    
-	// TODO Bind service, add textView, downloadButton, sortingButton, broadcastReceiver
+		@Override
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			// We've bound to LocalService, cast the IBinder and get
+			// LocalService instance
+			LocalBinder binder = (LocalBinder) service;
+			mService = binder.getService();
+			mBound = true;
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName arg0) {
+			mBound = false;
+		}
+	};
+
+	// TODO add textView, downloadButton, sortingButton, broadcastReceiver
 }
