864b60dcc8aa77c321de6d503bb7db849386e32e
René Nilsson <renenilsson01@gmail.com>
2013-05-14 15:10:20 +0200

    Hand In 4 Done!

--- Hand_in4/src/com/kn10731/iha/handin4/DownloadService.java
+++ Hand_in4/src/com/kn10731/iha/handin4/DownloadService.java
@@ -2,11 +2,10 @@ package com.kn10731.iha.handin4;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.List;
+import java.util.ArrayList;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -23,13 +22,15 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 public class DownloadService extends Service {
 
-	private final String TAG = "downloadService";
+	private final String downloadIntent = "com.kn10731.iha.handin4.downloadIntent";
+	private final String TAG = "DownloadService";
 	private final IBinder mBinder = new LocalBinder();
-	List<String> sTogList = null;
+	ArrayList<String> sTogList = new ArrayList<String>();
 
 	public class LocalBinder extends Binder {
 		DownloadService getService() {
@@ -42,8 +43,12 @@ public class DownloadService extends Service {
 		return mBinder;
 	}
 
-	public List<String> getDownloadedList() {
+	public ArrayList<String> getDownloadedList() {
 		return sTogList;
+	}
+
+	public void startDownload() {
+		new DownloadTask().execute("");
 	}
 
 	public class DownloadTask extends AsyncTask<String, Void, String> {
@@ -51,14 +56,13 @@ public class DownloadService extends Service {
 		@Override
 		protected String doInBackground(String... params) {
 
-			String result = null;
 			URI myURI = null;
 
 			try {
 				myURI = new URI(
 						"http://stog.itog.dk/itog/action/list/format/json");
 			} catch (URISyntaxException e) {
-				// Deal with it
+				Log.d(TAG, e.toString());
 			}
 			HttpClient httpClient = new DefaultHttpClient();
 			HttpGet getMethod = new HttpGet(myURI);
@@ -66,67 +70,46 @@ public class DownloadService extends Service {
 			try {
 				webServerResponse = httpClient.execute(getMethod);
 			} catch (ClientProtocolException e) {
+				Log.d(TAG, e.toString());
 			} catch (IOException e) {
+				Log.d(TAG, e.toString());
+			}
+
+			if (webServerResponse == null) {
+				return null;
 			}
 
 			HttpEntity httpEntity = webServerResponse.getEntity();
 
 			if (httpEntity != null) {
-				InputStream instream;
 				try {
-					instream = httpEntity.getContent();
-					result = convertStreamToString(instream);
-					instream.close();
-				} catch (IllegalStateException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					BufferedReader in = new BufferedReader(
+							new InputStreamReader(httpEntity.getContent()));
+
+					StringBuilder SB = new StringBuilder();
+					String line;
+					while ((line = in.readLine()) != null) {
+						SB.append(line);
+					}
+
+					JSONArray ja = new JSONArray(SB.toString());
+					sTogList.clear();
+					for (int i = 0; i < ja.length(); i++) {
+						JSONObject jo = (JSONObject) ja.get(i);
+						sTogList.add(jo.getString("name"));
+//						Log.d(TAG, jo.getString("name"));
+					}
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					Log.d(TAG, e.toString());
+				} catch (JSONException e) {
+					Log.d(TAG, e.toString());
 				}
-
 			}
 
-			JSONObject myJSON = null;
-
-			try {
-				myJSON = new JSONObject(result);
-				JSONArray array = myJSON.getJSONArray("items");
-				sTogList.clear();
-
-				for (int i = 0; i < array.length(); i++) {
-					JSONObject sTogJSON = array.getJSONObject(i);
-
-					String name = sTogJSON.getString("name");
-					sTogList.add(name);
-					Log.d(TAG, name);
-
-				}
-			} catch (JSONException e) {
-			}
-
-			// send Broadcast
+			Intent intent = new Intent(downloadIntent);
+			LocalBroadcastManager.getInstance(getBaseContext()).sendBroadcast(
+					intent);
 			return null;
-		}
-	}
-
-	public String convertStreamToString(InputStream is) throws IOException {
-		if (is != null) {
-			StringBuilder sb = new StringBuilder();
-			String line = null;
-
-			try {
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(is, "UTF-8"));
-				while ((line = reader.readLine()) != null) {
-					sb.append(line).append("\n");
-				}
-			} finally {
-				is.close();
-			}
-			return sb.toString();
-		} else {
-			return "";
 		}
 	}
 }
--- Hand_in4/src/com/kn10731/iha/handin4/MainActivity.java
+++ Hand_in4/src/com/kn10731/iha/handin4/MainActivity.java
@@ -1,34 +1,36 @@
 package com.kn10731.iha.handin4;
 
+import java.util.ArrayList;
+
 import android.app.ListActivity;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.kn10731.iha.handin4.DownloadService.LocalBinder;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends ListActivity implements OnClickListener {
 
+	private final String downloadIntent = "com.kn10731.iha.handin4.downloadIntent";
+	private final String TAG = "MainActivity";
+	private ArrayAdapter<String> adapter = null;
+	private EditText filterText = null;
 	DownloadService mService;
 	boolean mBound = false;
-
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	protected void onStart() {
-		super.onStart();
-		// Bind to LocalService
-		Intent intent = new Intent(this, DownloadService.class);
-		bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-	}
 
 	/** Defines callbacks for service binding, passed to bindService() */
 	private ServiceConnection mConnection = new ServiceConnection() {
@@ -48,5 +50,72 @@ public class MainActivity extends ListActivity {
 		}
 	};
 
-	// TODO add textView, downloadButton, sortingButton, broadcastReceiver
+	private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Log.d(TAG, "Received broadcast");
+			ArrayList<String> list = mService.getDownloadedList();
+
+			if (!list.isEmpty()) {
+				adapter = new ArrayAdapter<String>(getApplicationContext(),
+						R.layout.list_layout, android.R.id.text1, list);
+				setListAdapter(adapter);
+			} else {
+				Log.d(TAG, "List is empty.");
+			}
+		}
+	};
+
+	private TextWatcher filterTextWatcher = new TextWatcher() {
+
+		public void afterTextChanged(Editable s) {
+		}
+
+		public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+		}
+
+		public void onTextChanged(CharSequence s, int start, int before,
+				int count) {
+			if (adapter != null) {
+				adapter.getFilter().filter(s);
+			}
+		}
+	};
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		LocalBroadcastManager.getInstance(this).registerReceiver(
+				mMessageReceiver, new IntentFilter(downloadIntent));
+
+		Button updateButton = (Button) findViewById(R.id.button1);
+		updateButton.setOnClickListener(this);
+
+		filterText = (EditText) findViewById(R.id.editText1);
+		filterText.addTextChangedListener(filterTextWatcher);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// Bind to LocalService
+		Intent intent = new Intent(this, DownloadService.class);
+		bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+	}
+
+	@Override
+	public void onClick(View v) {
+		Log.d(TAG, "Starting download");
+		mService.startDownload();
+	}
+
+	@Override
+	protected void onDestroy() {
+		filterText.removeTextChangedListener(filterTextWatcher);
+		unbindService(mConnection);
+		super.onDestroy();
+	}
 }
