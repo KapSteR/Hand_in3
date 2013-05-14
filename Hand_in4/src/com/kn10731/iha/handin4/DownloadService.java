c5f0c49294c5d5b7c3a21288350235a3a2440c1d
René Nilsson <renenilsson01@gmail.com>
2013-05-14 09:37:39 +0200

    Added downloadService

---
+++ Hand_in4/src/com/kn10731/iha/handin4/DownloadService.java
@@ -0,0 +1,124 @@
+package com.kn10731.iha.handin4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class DownloadService extends Service {
+	
+	private final String TAG = "downloadService";
+	private final IBinder mBinder = new LocalBinder();
+	List<String> sTogList = null;
+	
+	public class LocalBinder extends Binder {
+		DownloadService getService() {
+			return DownloadService.this;
+		}
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return mBinder;
+	}
+
+	public void downloadList() {  //must run on a background thread
+
+		String result = null;
+		URI myURI = null;
+
+		try {
+			myURI = new URI("http://stog.itog.dk/itog/action/list/format/json");
+		} catch (URISyntaxException e) {
+			// Deal with it
+		}
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpGet getMethod = new HttpGet(myURI);
+		HttpResponse webServerResponse = null;
+		try {
+			webServerResponse = httpClient.execute(getMethod);
+		} catch (ClientProtocolException e) {
+		} catch (IOException e) {
+		}
+
+		HttpEntity httpEntity = webServerResponse.getEntity();
+
+		if (httpEntity != null) {
+			InputStream instream;
+			try {
+				instream = httpEntity.getContent();
+				result = convertStreamToString(instream);
+				instream.close();
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		JSONObject myJSON = null;
+
+		try {
+			myJSON = new JSONObject(result);
+			JSONArray array = myJSON.getJSONArray("items");
+			sTogList.clear();
+			
+			for (int i = 0; i < array.length(); i++) {
+				JSONObject sTogJSON = array.getJSONObject(i);
+
+				String name = sTogJSON.getString("name");
+				sTogList.add(name);
+				Log.d(TAG, name);
+
+			}
+		} catch (JSONException e) {
+		}
+
+	}
+	
+	public List<String> getDownloadedList(){
+		return sTogList;
+	}
+
+	public String convertStreamToString(InputStream is) throws IOException {
+		if (is != null) {
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+
+			try {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(is, "UTF-8"));
+				while ((line = reader.readLine()) != null) {
+					sb.append(line).append("\n");
+				}
+			} finally {
+				is.close();
+			}
+			return sb.toString();
+		} else {
+			return "";
+		}
+	}
+}
--- Hand_in4/src/com/kn10731/iha/handin4/MainActivity.java
+++ Hand_in4/src/com/kn10731/iha/handin4/MainActivity.java
@@ -1,22 +1,54 @@
 package com.kn10731.iha.handin4;
 
+import android.app.ListActivity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.os.IBinder;
 
-public class MainActivity extends Activity {
+import com.kn10731.iha.handin4.DownloadService.LocalBinder;
 
+public class MainActivity extends ListActivity {
+	
+	DownloadService mService;
+    boolean mBound = false;
+	
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService
+        Intent intent = new Intent(this, DownloadService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+	
+	
+	 /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
 
+        @Override
+        public void onServiceConnected(ComponentName className,
+                IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            LocalBinder binder = (LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+    
+    
+	// TODO Bind service, add textView, downloadButton, sortingButton, broadcastReceiver
 }
