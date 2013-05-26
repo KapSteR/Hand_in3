7639884f89922ff0e3465015ffb175d59134b1ba
Kasper Nielsen <the.kapster@gmail.com>
2013-05-26 17:43:59 +0200

    Added post code validation
    Added code to Position.setPostCode in order to eliminate "Desværre ingen
    udsigt for valgte by" error in "Byvejr" mode.

--- ThemeProject/src/com/itsmap/kn10731/themeproject/simpledmiapp/Position.java
+++ ThemeProject/src/com/itsmap/kn10731/themeproject/simpledmiapp/Position.java
@@ -4,8 +4,8 @@ import android.content.Context;
 import android.util.Log;
 
 public class Position {
-	
-	// Definitions for politikreds ID 
+
+	// Definitions for politikreds ID
 	private static final int LOCALE_NORDJ = 1;
 	private static final int LOCALE_OSTJ = 2;
 	private static final int LOCALE_MIDTVJ = 3;
@@ -18,41 +18,79 @@ public class Position {
 	private static final int LOCALE_KBHV = 10;
 	private static final int LOCALE_KBH = 11;
 	private static final int LOCALE_BORN = 12;
-	
+
 	private String city = null;
-	
+
 	Context context;
-	
-	public Position(Context context){
+
+	public Position(Context context) {
 		this.context = context;
 	}
-	
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
-	
-	
+
 	private String postCode = null;
+
 	public String getPostCode() {
 		return postCode;
 	}
+
 	public void setPostCode(String postCode) {
-		this.postCode = postCode;
+		
+		int postInt = Integer.valueOf(postCode);
+		String temp = null;
+		   
+		if (postInt < 1800)
+			temp = "1000";
+
+		else if (postInt < 2000) 
+			temp = "2000";
+
+		else if (postInt > 2000 && postInt < 2500) 
+			temp = "1000";
+
+		else if (postInt > 5000 && postInt < 5280)
+			temp = "5000";
+
+		else if (postInt > 6000 && postInt < 6020)
+			temp = "6000";
+
+		else if (postInt > 6700 && postInt < 6720)
+			temp = "6700";
+
+		else if (postInt > 7100 && postInt < 7130)
+			temp = "7100";
+
+		else if (postInt > 8000 && postInt < 8220)
+			temp = "8000";
+
+		else if (postInt == 8920 || postInt == 8930 || postInt == 8940 || postInt == 8960)
+			temp = "8900";
+
+		else if (postInt > 9000 && postInt < 9230)
+			temp = "9000";
+
+		else if (postInt > 9999) 
+			temp = "1000";
+		
+		this.postCode = temp;
 	}
 
-	
 	private String region = null;
 	private String textName = null;
 	private String pngName = null;
 
 	public void setRegion(int regionIndex) {
-		
+
 		switch (regionIndex) {
 		case LOCALE_NORDJ:
-			
+
 			this.region = context.getString(R.string.nordj);
 			this.pngName = context.getString(R.string.png_nordj);
 			this.textName = context.getString(R.string.text_nordj);
@@ -119,12 +157,12 @@ public class Position {
 
 	public String getRegion() {
 		return region;
-	}	
-	
+	}
+
 	public String getTextName() {
 		return textName;
 	}
-	
+
 	public String getPngName() {
 		return pngName;
 	}
